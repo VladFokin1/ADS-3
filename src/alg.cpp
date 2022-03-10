e44b01549fa3cd548ec1ce8e8b560871b024207c
VladFokin1 <94217312+VladFokin1@users.noreply.github.com>
2022-03-10 20:22:43 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,9 +4,21 @@ int cbinsearch(int *arr, int size, int value) {
     int i = 0, j = size - 1, count  = 0; // i - левая граница массива, j - правая, count - счетчик
     while(i < j) {
         int mid = i + (j - i) / 2;
-        if(arr[mid] == value)
-            count+=1;
-        else if(arr[mid] > number)
+        if(arr[mid] == value) {
+            //count+=1;
+            i = mid;
+            while (arr[i] == value) {
+                i--;
+            }
+            while (arr[j] != value) {
+                j--;
+            }
+            while (i < j) {
+                count++;
+                i++;
+            }
+        }
+        else if(arr[mid] > value)
             j = mid;
         else
             i = mid + 1;
