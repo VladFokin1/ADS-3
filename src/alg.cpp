8c9fcf433a2782e660de097a14ba40ff0af20ad8
VladFokin1 <94217312+VladFokin1@users.noreply.github.com>
2022-03-10 19:48:16 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,15 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+    int i = 0, j = size - 1, count  = 0; // i - левая граница массива, j - правая, count - счетчик
+    while(i < j) {
+        int mid = i + (j - i) / 2;
+        if(arr[mid] == value)
+            count+=1;
+        else if(arr[mid] > number)
+            j = mid;
+        else
+            i = mid + 1;
+    }
+    return count;
 }
